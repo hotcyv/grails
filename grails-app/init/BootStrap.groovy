38e486347dc08b54d71b3b83c97c06dd0fefa220
Victor <hotcyv@gmail.com>
2015-08-10 20:37:09 -0300

    JSON template changed

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,13 +1,37 @@
 package tarefas
+import grails.converters.JSON
 
 class BootStrap {
 
     def init = { servletContext ->
-        //new Tarefa(nome:"Estudar OpenData...",deadLine:new Date(),completed:true,categoria: new Categoria(nome:"Mestrado")).save()
-        //new Tarefa(nome:"Estudar Música...",deadLine:new Date(),completed:false,categoria: new Categoria(nome:"Hobby")).save()
-        new Tarefa(nome:"Estudar Mestrado...",deadLine:"12/12/2016",complete:false,categoria:"Pessoal").save()
-        new Tarefa(nome:"Estudar Música...",deadLine:"08/08/2015",complete:false,categoria:"Profissional").save()
-        
+
+        JSON.registerObjectMarshaller(Tarefa) {
+            def returnArray = [:]
+            returnArray['id'] = it.id
+            returnArray['nome'] = it.nome
+            returnArray['deadLine'] = it.deadLine
+            returnArray['complete'] = it.complete
+            returnArray['categoria'] = it.categoria.id
+            returnArray['categoriaNome'] = it.categoria.nome
+            return returnArray
+        }
+
+        JSON.registerObjectMarshaller(Categoria) {
+            def returnArray = [:]
+            returnArray['id'] = it.id
+            returnArray['nome'] = it.nome
+            return returnArray
+        }
+        new Categoria(nome:"Profissional").save()
+        new Categoria(nome:"Pessoal").save()
+	    def c1 = new Categoria(nome: "Hooby")
+        .addToTarefas(new Tarefa(nome:"Estudar Música...",deadLine:"2015-06-04",complete:"false"))
+        .addToTarefas(new Tarefa(nome:"Estudar mestrado...",deadLine:"2015-08-09",complete:"true"))
+        c1.save()
+        if(c1.hasErrors()){
+	       println c1.errors
+	    }
+
     }
     def destroy = {
     }
